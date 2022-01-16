263d1e074d2171867ca089b5e8da781ee2c2bda6
Dundappa253 <appa253@gmail.com>
2022-01-16 13:59:38 +0530

    Removed jpa code

--- src/main/java/com/motagifarming/grapesmarket/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.motagifarming.grapesmarket.repository;
-
-import com.motagifarming.grapesmarket.enitity.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-
-}
